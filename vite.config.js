62fa4a57bebfc806a0da3ba952c1654a04a6dbaf
gajeranirjar <gnirjar1234@gmail.com>
2025-05-14 21:54:42 +0530

    make pwa app

--- vite.config.js
+++ vite.config.js
@@ -19,13 +19,13 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/favicon-16x16.png',
-            sizes: '16x16',
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/favicon-32x32.png',
-            sizes: '32x32',
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
             type: 'image/png'
           }
         ]
