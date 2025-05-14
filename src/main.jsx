2f103ddf30be9403042a5e0717f4511cd180e914
gajeranirjar <gnirjar1234@gmail.com>
2025-05-14 16:21:30 +0530

    Make Progressive web app

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('New version available. Reload?')) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline');
+  }
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Nirjar Gajera Weather App',
+        short_name: 'NirjNext💖⛅',
+        description: 'A simple weather PWA',
+        theme_color: '#438d95',
+        background_color: '#2f2f2f',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/favicon-16x16.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/favicon-32x32.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }) 
+  ],
 })
